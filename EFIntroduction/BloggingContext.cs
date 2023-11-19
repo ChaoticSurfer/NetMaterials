c37df6b3500137b2c652f62e3813bf72c270d0de
anri <anri.kezeroti.1@btu.edu.ge>
2023-11-20 00:02:25 +0400

    EF Introduction

---
+++ EFIntroduction/BloggingContext.cs
@@ -0,0 +1,31 @@
+﻿using EFIntroduction.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFIntroduction
+{
+    public class BloggingContext: DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        public string DbPath { get; }
+
+        public BloggingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source = {DbPath}");
+        }
+    }
+}
---
+++ EFIntroduction/README.md
@@ -0,0 +1,5 @@
+
+### Entity Framework Core
+
+- **Overview**: Learn the basics of Entity Framework Core, a popular ORM for .NET. This tutorial guides you through the process of creating your first application using EF Core.
+- **Access the Tutorial**: [Getting Started with EF Core](https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli)
